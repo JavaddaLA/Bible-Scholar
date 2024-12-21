5ec1269d375c4b73b82a24b00c1bc146c424895f
javaddala <javaddala@gmail.com>
2024-12-20 19:18:42 -0800

    init commit

--- app.py
+++ app.py
@@ -12,4 +12,22 @@ st.header("Learning the Word")
 instructions = st.text_area(
     "Ask me a theological question? "
 )
+if (instructions) < 1000:
+    if st.button("Show Options"):
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "Act like a theologian that is well versed in bible studies and doctrines"},
+                {"role": "user", "content": instructions}
+            ],
+            temperature=0,
+            max_tokens=1000,
+         )
 
+        output = response.choices[0].message.content 
+        # Access the message content correctly
+
+else:
+        st.warning (
+             "Query too long. Write a shorter query."
+        )
